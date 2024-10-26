e53e123911a9e75ae4123c9bcf1e26d1d067a623
Karan Bhatt <prakaranbhatt@gmail.com>
2024-10-26 08:45:30 +0545

    Display critic score and get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,13 +1,23 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGame";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 const GameCard = (game: Game) => {
-  const { name, background_image } = game;
+  const { name, background_image, parent_platforms, metacritic } = game;
+
   return (
     <Card overflow={"hidden"} borderRadius={10}>
-      <Image src={background_image} />
+      <Image src={getCroppedImageUrl(background_image)} />
       <CardBody>
         <Heading fontSize={"2xl"}>{name}</Heading>
+        <HStack justifyContent={"space-between"}>
+          <PlatformIconList
+            platforms={parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );

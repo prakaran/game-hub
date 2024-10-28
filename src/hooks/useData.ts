7299812d1ff151ff1ae42d38f5d321c874b0659e
Karan Bhatt <prakaranbhatt@gmail.com>
2024-10-28 10:54:32 +0545

    Build platform selector

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -9,7 +9,7 @@ interface FetchResponse<T> {
 const useData = <T>(
   endpoint: string,
   requestConfig?: AxiosRequestConfig,
-  deps: any
+  deps?: any
 ) => {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");

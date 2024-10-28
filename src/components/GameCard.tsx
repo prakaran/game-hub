7a5b19eda58da306288e11bab1e970c9e213b17b
Karan Bhatt <prakaranbhatt@gmail.com>
2024-10-29 01:19:06 +0545

    Add Emoji

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,9 +3,11 @@ import { Game } from "../hooks/useGame";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
 
 const GameCard = (game: Game) => {
-  const { name, background_image, parent_platforms, metacritic } = game;
+  const { name, background_image, parent_platforms, metacritic, rating_top } =
+    game;
 
   return (
     <Card overflow={"hidden"} borderRadius={10}>
@@ -17,7 +19,10 @@ const GameCard = (game: Game) => {
           />
           <CriticScore score={metacritic} />
         </HStack>
-        <Heading fontSize={"2xl"}>{name}</Heading>
+        <Heading fontSize={"2xl"}>
+          {name}
+          <Emoji rating={rating_top} />
+        </Heading>
       </CardBody>
     </Card>
   );
